5648d55f9525d3203b413e4a9f034251628b453e
R.D Chathuranga <140147196+dimuth12345@users.noreply.github.com>
2023-08-03 11:41:42 +0530

    Initiated

---
+++ addtocart.php
@@ -0,0 +1,24 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "ap");
+
+// Check if the connection was successful
+
+
+if(isset ($_POST['cart'])){
+    $pname = $_POST['p_name'];
+    $p_featered_photo=$_POST['p_featured_photo'];
+    $p_size=$_POST['size'];
+    $p_color=$_POST['color'];
+    $qty=$_POST['qty'];
+    $p_price=$_POST['price'];
+
+
+    $sqlQuery = "INSERT INTO cart ( `p_name`, `p_featured_photo`, `p_price`, `size`, `colour`, `qty`) VALUES('$pname','$p_featered_photo','$p_price','$p_size','$p_color','$qty')";
+    if (!mysqli_query($conn, $sqlQuery)) {
+        echo "Error: " . $sqlQuery . "<br>" . mysqli_error($conn);
+      }
+    header("Location: ./cart.php");
+
+}
+
+
